33ca77dd7de01cea367acfd51fd8771578af595d
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-07-06 15:41:23 +0500

    feat: add .gitignore file and create databae connection

---
+++ config/db.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnDB() (*sql.DB, error) {
+
+	// get data from .env file
+	db_user := os.Getenv("DB_USER")
+	db_pass := os.Getenv("DB_PASS")
+	db_host := os.Getenv("DB_HOST")
+	db_name := os.Getenv("DB_NAME")
+	db_port := os.Getenv("DB_PORT")
+
+	// connect database
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", db_host, db_port, db_user, db_pass, db_name)
+	DB, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return DB, nil
+
+}
