e2d7e6ce586abf6c05baaede0aacecd88c4dd483
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-07-07 10:59:44 +0500

    feat: add routes

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"github/abbgo/yenil_yol/backend/config"
+	"github/abbgo/yenil_yol/backend/routes"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Nesip bolsa gelejegmin passiw geliri bolar")
+	// initialize database connection
+	db, err := config.ConnDB()
+	if err != nil {
+		db.Close()
+		log.Fatal(err)
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	r := routes.Routes()
+
+	// static file
+	os.Mkdir("./uploads", os.ModePerm)
+	r.Static("/uploads", "./uploads")
+
+	// run routes
+	if err := r.Run(":7723"); err != nil {
+		log.Fatal(err)
+	}
+
 }
---
+++ routes/api.go
@@ -0,0 +1,53 @@
+package routes
+
+import (
+	// backApi "github/abbgo/isleg/isleg-backend/routes/back"
+	frontApi "github/abbgo/yenil_yol/backend/routes/front"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/gzip"
+	"github.com/gin-gonic/gin"
+)
+
+func Routes() *gin.Engine {
+
+	routes := gin.Default()
+
+	// cors
+	// routes.Use(cors.Default())
+
+	routes.Use(gzip.Gzip(gzip.DefaultCompression))
+
+	routes.Use(cors.New(cors.Config{
+		// AllowOrigins:     []string{"https://foo.com"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "RefreshToken", "Authorization"},
+		AllowCredentials: true,
+		AllowAllOrigins:  true,
+		// MaxAge:           12 * time.Hour,
+	}))
+
+	// routes belong to admin panel
+	// back := routes.Group("/api/admin")
+	// {
+	// 	backApi.AdminRoutes(back)
+
+	// 	// bu group - daki ahli yazylan funksiyalary dine ahli adminler isledip biler
+	// 	// CheckAdmin middleware sony kesgitleyar
+	// 	backApi.BackRoutes(back)
+
+	// }
+
+	front := routes.Group("/api")
+	{
+		// customer routes
+		// frontApi.CustomerRoutes(front)
+
+		// bu group - a degisli api - lerden maglumat alynanda ( :lang ) paramter boyunca uytgedilip
+		// terjime alynyar
+		frontApi.FrontRoutes(front)
+	}
+
+	return routes
+
+}
