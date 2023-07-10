f65d8f9c8ce0e82d46f2a4c95913f6c1dbebf4c8
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-07-10 16:38:28 +0500

    fix: update Shop model

--- models/shop.go
+++ models/shop.go
@@ -1,26 +1,27 @@
 package models
 
 type Shop struct {
-	ID          string  `json:"id,omitempty"`
-	NameTM      string  `json:"name_tm,omitempty" binding:"required"`
-	NameRU      string  `json:"name_ru,omitempty" binding:"required"`
-	Address     string  `json:"address,omitempty" binding:"required"`
-	Latitude    float32 `json:"latitude,omitempty" binding:"required"`
-	Longitude   float32 `json:"longitude,omitempty" binding:"required"`
-	Image       string  `json:"image,omitempty"`
-	HasDelivery bool    `json:"has_delivery,omitempty" binding:"required"`
-	ShopOwnerID string  `json:"shop_owner_id,omitempty" binding:"required"`
-	SlugTM      string  `json:"slug_tm,omitempty"`
-	SlugRU      string  `json:"slug_ru,omitempty"`
-	CreatedAt   string  `json:"-"`
-	UpdatedAt   string  `json:"-"`
-	DeletedAt   string  `json:"-"`
+	ID          string      `json:"id,omitempty"`
+	NameTM      string      `json:"name_tm,omitempty" binding:"required"`
+	NameRU      string      `json:"name_ru,omitempty" binding:"required"`
+	Address     string      `json:"address,omitempty" binding:"required"`
+	Latitude    float32     `json:"latitude,omitempty" binding:"required"`
+	Longitude   float32     `json:"longitude,omitempty" binding:"required"`
+	Image       string      `json:"image,omitempty"`
+	HasDelivery bool        `json:"has_delivery,omitempty" binding:"required"`
+	ShopOwnerID string      `json:"shop_owner_id,omitempty" binding:"required"`
+	SlugTM      string      `json:"slug_tm,omitempty"`
+	SlugRU      string      `json:"slug_ru,omitempty"`
+	ShopPhones  []ShopPhone `json:"shop_phones" binding:"required"`
+	CreatedAt   string      `json:"-"`
+	UpdatedAt   string      `json:"-"`
+	DeletedAt   string      `json:"-"`
 }
 
 type ShopPhone struct {
 	ID          string `json:"id,omitempty"`
 	PhoneNumber string `json:"phone_number,omitempty" binding:"required"`
-	ShopID      string `json:"shop_id,omitempty" binding:"required"`
+	ShopID      string `json:"shop_id,omitempty"`
 	CreatedAt   string `json:"-"`
 	UpdatedAt   string `json:"-"`
 	DeletedAt   string `json:"-"`
