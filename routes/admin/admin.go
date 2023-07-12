c9f5e8a1b121418ec913007031290503212cc8c8
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-07-12 16:26:48 +0500

    feat: add admin controller

--- routes/admin/admin.go
+++ routes/admin/admin.go
@@ -1,1 +1,46 @@
 package adminApi
+
+import (
+	controllers "github/abbgo/yenil_yol/backend/controllers/admin"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AdminRoutes(back *gin.RouterGroup) {
+
+	admin := back.Group("/admins")
+	{
+		{
+			// RegisterAdmin admin - i registrasiya etmek ucin ulanylyar.
+			admin.POST("register", controllers.RegisterAdmin)
+			// 	// admin.POST("register", middlewares.IsSuperAdmin(), adminController.RegisterAdmin)
+
+			// 	// LoginShopOwner shop_owner - i login etmek ucin ulanylyar.
+			// 	shopOwner.POST("login", controllers.LoginShopOwner)
+
+			// 	// UpdateShopOwner shop_owner - in maglumatlaryny uytgetmek ucin ulanylyar.
+			// 	shopOwner.PUT("update", controllers.UpdateShopOwner)
+
+			// 	// ShopOwner - in access tokenin tazelelap refresh bilen access tokeni bile bermek
+			// 	// ucin ulanylyar
+			// 	shopOwner.POST("refresh", helpers.RefreshTokenForAdmin)
+
+			// 	// GetShopOwner funksiya haeder - den gelen id boyunca bir sany shop_owneri almak ucin ulanylyar.
+			// 	shopOwner.GET("one", controllers.GetShopOwner)
+
+			// 	// GetShopOwners funksiya hemme shop_owner - leri almak ucin ulanylyar.
+			// 	shopOwner.GET("", controllers.GetShopOwners)
+
+			// 	// DeleteShopOwnerByID funksiya id boyunca shop_owner - i korzina salmak ucin ulanylyar.
+			// 	shopOwner.DELETE(":id", controllers.DeleteShopOwnerByID)
+
+			// 	// RestoreShopOwnerByID funksiya id boyunca shop_owner - i korzinadan cykarmak ucin ulanylyar.
+			// 	shopOwner.GET(":id/restore", controllers.RestoreShopOwnerByID)
+
+			// 	// DeletePermanentlyShopOwnerByID funksiya id boyunca shop_owner - i doly ( korzinadan ) pozmak ucin ulanylyar
+			// 	shopOwner.DELETE(":id/delete", controllers.DeletePermanentlyShopOwnerByID)
+
+		}
+	}
+
+}
