4eb2897c4313c8d4c119422696c928589230e9d6
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-07-17 20:36:50 +0500

    fix: update PageTranslation model

--- models/page_translation.go
+++ models/page_translation.go
@@ -2,10 +2,10 @@ package models
 
 type PageTranslation struct {
 	ID            string `json:"id,omitempty"`
-	TitleTM       string `json:"title_tm,omitempty" binding:"required"`
-	TitleRU       string `json:"title_ru,omitempty" binding:"required"`
-	DescriptionTM string `json:"description_tm,omitempty" binding:"required"`
-	DescriptionRU string `json:"description_ru,omitempty" binding:"required"`
+	TitleTM       string `json:"title_tm,omitempty"`
+	TitleRU       string `json:"title_ru,omitempty"`
+	DescriptionTM string `json:"description_tm,omitempty"`
+	DescriptionRU string `json:"description_ru,omitempty"`
 	PageID        string `json:"page_id,omitempty" binding:"required"`
 	CreatedAt     string `json:"-"`
 	UpdatedAt     string `json:"-"`
