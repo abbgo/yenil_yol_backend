c2b7aef819e685b634424281f132662ea1eefc35
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-07-17 17:53:25 +0500

    feat: add Page model

---
+++ models/page.go
@@ -0,0 +1,10 @@
+package models
+
+type Page struct {
+	ID        string `json:"id,omitempty"`
+	Name      string `json:"name,omitempty" binding:"required"`
+	Image     string `json:"image,omitempty"`
+	CreatedAt string `json:"-"`
+	UpdatedAt string `json:"-"`
+	DeletedAt string `json:"-"`
+}
