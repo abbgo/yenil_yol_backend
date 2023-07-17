e1ba2848528d9eb4f980a0a3eab67624d6a96c53
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-07-17 14:31:57 +0500

    feat: add Product model

---
+++ models/product.go
@@ -0,0 +1,22 @@
+package models
+
+type Product struct {
+	ID         string  `json:"id,omitempty"`
+	NameTM     string  `json:"name_tm,omitempty" binding:"required"`
+	NameRU     string  `json:"name_ru,omitempty" binding:"required"`
+	Image      string  `json:"image,omitempty" binding:"required"`
+	Price      float32 `json:"price,omitempty" binding:"required"`
+	OldPrice   float32 `json:"old_price,omitempty"`
+	Status     bool    `json:"status,omitempty"`
+	Color      string  `json:"color,omitempty" binding:"required"`
+	Gender     string  `json:"gender,omitempty" binding:"required"`
+	Code       string  `json:"code,omitempty"`
+	SlugTM     string  `json:"slug_tm,omitempty"`
+	SlugRU     string  `json:"slug_ru,omitempty"`
+	ShopID     string  `json:"shop_id,omitempty" binding:"required"`
+	CategoryID string  `json:"category_id,omitempty" binding:"required"`
+	BrendID    string  `json:"brend_id,omitempty" binding:"required"`
+	CreatedAt  string  `json:"-"`
+	UpdatedAt  string  `json:"-"`
+	DeletedAt  string  `json:"-"`
+}
