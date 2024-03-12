fdf5e81d781c62f9fa2b9beeddcc18d1270dab94
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-03-12 17:38:17 +0500

    fix: some commit

--- controllers/back/product.go
+++ controllers/back/product.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"github/abbgo/yenil_yol/backend/config"
 	"github/abbgo/yenil_yol/backend/helpers"
 	"github/abbgo/yenil_yol/backend/models"
@@ -41,7 +40,6 @@ func CreateProduct(c *gin.Context) {
 	_, err = db.Exec(context.Background(), "INSERT INTO products (name_tm,name_ru,image,price,old_price,status,color_name_tm,color_name_ru,gender_name_tm,gender_name_ru,code,slug_tm,slug_ru,shop_id,category_id,brend_id) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16)", product.NameTM, product.NameRU, product.Image, product.Price, product.OldPrice, product.Status, product.ColorNameTM, product.ColorNameRU, product.GenderNameTM, product.GenderNameRU, product.Code, slug.MakeLang(product.NameTM, "en"), slug.MakeLang(product.NameRU, "en"), product.ShopID, product.CategoryID, product.BrendID)
 	if err != nil {
 		helpers.HandleError(c, 400, err.Error())
-		fmt.Println("yalnys 1")
 		return
 	}
 
