adaff6f2ee1b65ab7900c2dcd90b4b659cf0574d
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-03-13 16:51:59 +0500

    feat: add dimension_group_id column to Category model

--- models/category.go
+++ models/category.go
@@ -1,15 +1,16 @@
 package models
 
 type Category struct {
-	ID        string `json:"id,omitempty"`
-	NameTM    string `json:"name_tm,omitempty" binding:"required"`
-	NameRU    string `json:"name_ru,omitempty" binding:"required"`
-	Image     string `json:"image,omitempty"`
-	SlugTM    string `json:"slug_tm,omitempty"`
-	SlugRU    string `json:"slug_ru,omitempty"`
-	CreatedAt string `json:"-"`
-	UpdatedAt string `json:"-"`
-	DeletedAt string `json:"-"`
+	ID               string `json:"id,omitempty"`
+	NameTM           string `json:"name_tm,omitempty" binding:"required"`
+	NameRU           string `json:"name_ru,omitempty" binding:"required"`
+	Image            string `json:"image,omitempty"`
+	SlugTM           string `json:"slug_tm,omitempty"`
+	SlugRU           string `json:"slug_ru,omitempty"`
+	DimensionGroupID string `json:"dimension_group_id,omitempty" binding:"required"`
+	CreatedAt        string `json:"-"`
+	UpdatedAt        string `json:"-"`
+	DeletedAt        string `json:"-"`
 }
 
 type CategoryUpdate struct {
--- models/dimension_groups.go
+++ models/dimension_groups.go
@@ -2,7 +2,7 @@ package models
 
 type DimensionGroups struct {
 	ID        string `json:"id,omitempty"`
-	Dimension string `json:"dimension" binding:"required"`
+	Name      string `json:"name" binding:"required"`
 	CreatedAt string `json:"-"`
 	UpdatedAt string `json:"-"`
 	DeletedAt string `json:"-"`
