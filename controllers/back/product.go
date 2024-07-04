5ee7bea2bf6ac922567a94a7779872a1d9f32bc3
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-04 13:23:15 +0500

    fix: update GetProductByID function

--- controllers/back/product.go
+++ controllers/back/product.go
@@ -251,22 +251,22 @@ func GetProductByID(c *gin.Context) {
 			productColor.Dimensions = append(productColor.Dimensions, dimension)
 		}
 
-		// renk alynandan son sol renke degisli suratlar alynyar
-		rowsImage, err := db.Query(context.Background(), "SELECT image FROM product_images WHERE product_color_id=$1 AND deleted_at IS NULL", productColor.ID)
-		if err != nil {
-			helpers.HandleError(c, 400, err.Error())
-			return
-		}
-		defer rowsImage.Close()
+		// // renk alynandan son sol renke degisli suratlar alynyar
+		// rowsImage, err := db.Query(context.Background(), "SELECT image FROM product_images WHERE product_color_id=$1 AND deleted_at IS NULL", productColor.ID)
+		// if err != nil {
+		// 	helpers.HandleError(c, 400, err.Error())
+		// 	return
+		// }
+		// defer rowsImage.Close()
 
-		for rowsImage.Next() {
-			var image string
-			if err := rowsImage.Scan(&image); err != nil {
-				helpers.HandleError(c, 400, err.Error())
-				return
-			}
-			productColor.Images = append(productColor.Images, image)
-		}
+		// for rowsImage.Next() {
+		// 	var image string
+		// 	if err := rowsImage.Scan(&image); err != nil {
+		// 		helpers.HandleError(c, 400, err.Error())
+		// 		return
+		// 	}
+		// 	productColor.Images = append(productColor.Images, image)
+		// }
 
 		product.ProductColors = append(product.ProductColors, productColor)
 	}
