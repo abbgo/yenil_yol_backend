e9f50ccd9bb1eab90c887b29010694569bfe3d70
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-04 13:17:53 +0500

    fix: add order number column to ProductImage model

--- models/product_image.go
+++ models/product_image.go
@@ -4,4 +4,5 @@ type ProductImage struct {
 	ID             string `json:"id,omitempty"`
 	Image          string `json:"image,omitempty"`
 	ProductColorID string `json:"product_color_id,omitempty"`
+	OrderNumber    int8   `json:"order_number,omitempty"`
 }
