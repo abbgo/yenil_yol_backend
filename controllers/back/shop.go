c1a79139a570de67da21aaf09958c4eb01306802
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-04 13:04:11 +0500

    fix: update UpdateShopByID function

--- controllers/back/shop.go
+++ controllers/back/shop.go
@@ -108,8 +108,10 @@ func UpdateShopByID(c *gin.Context) {
 
 	// database - daki maglumatlary request body - dan gelen maglumatlar bilen calysyas
 	_, err = db.Exec(context.Background(),
-		"UPDATE shops SET name_tm=$1 , name_ru=$2 , address_tm=$3 , address_ru=$4 , latitude=$5 , longitude=$6 , image=$7 , has_shipping=$8 , shop_owner_id=$9 , slug_tm=$10 , slug_ru=$11 , order_number=$12 , parent_shop_id=$13 , is_shopping_center=$14 WHERE id=$15",
-		shop.NameTM, shop.NameRU, shop.AddressTM, shop.AddressRU, shop.Latitude, shop.Longitude, shop.Image, shop.HasShipping, shop.ShopOwnerID, slug.MakeLang(shop.NameTM, "en"), slug.MakeLang(shop.NameRU, "en"), shop.OrderNumber, shop.ParentShopID, shop.IsShoppingCenter, shop.ID)
+		`UPDATE shops SET name_tm=$1 , name_ru=$2 , address_tm=$3 , address_ru=$4 , latitude=$5 , longitude=$6 , image=$7 , has_shipping=$8 , shop_owner_id=$9 , slug_tm=$10 , slug_ru=$11 , 
+		order_number=$12 , parent_shop_id=$13 , is_shopping_center=$14 , resized_image=$15 WHERE id=$16`,
+		shop.NameTM, shop.NameRU, shop.AddressTM, shop.AddressRU, shop.Latitude, shop.Longitude, shop.Image, shop.HasShipping, shop.ShopOwnerID, slug.MakeLang(shop.NameTM, "en"),
+		slug.MakeLang(shop.NameRU, "en"), shop.OrderNumber, shop.ParentShopID, shop.IsShoppingCenter, "assets/"+shop.Image.String, shop.ID)
 	if err != nil {
 		helpers.HandleError(c, 400, err.Error())
 		return
