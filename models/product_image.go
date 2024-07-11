f30732035ad8be285e4920d0b379e44c4951f8f6
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-07-11 05:56:32 +0500

    fix: update ProductImage model struct

--- models/product_image.go
+++ models/product_image.go
@@ -2,7 +2,7 @@ package models
 
 type ProductImage struct {
 	ID             string `json:"id,omitempty"`
-	Image          string `json:"image,omitempty"`
+	Image          string `json:"image,omitempty" binding:"required"`
 	ProductColorID string `json:"product_color_id,omitempty"`
-	OrderNumber    int8   `json:"order_number,omitempty"`
+	OrderNumber    int8   `json:"order_number,omitempty" binding:"required"`
 }
