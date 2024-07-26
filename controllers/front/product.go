44bf06c2a877cbd53e8fc308df0173ad5b8205f3
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-26 07:54:35 +0500

    fix: update GetProductsByIDs function

--- controllers/front/product.go
+++ controllers/front/product.go
@@ -302,7 +302,7 @@ func GetProductsByIDs(c *gin.Context) {
 		db.QueryRow(
 			context.Background(), `
 								SELECT pi.resized_image FROM product_images pi INNER JOIN product_colors pc ON pc.id=pi.product_color_id 
-								WHERE pc.product_id=$1 AND pi.deleted_at IS NULL AND pc.deleted_at IS NULL LIMIT 1
+								WHERE pc.product_id=$1 AND pc.order_number=1 AND pi.order_number=1 AND pi.deleted_at IS NULL AND pc.deleted_at IS NULL
 							`,
 			product.ID).Scan(&product.Image)
 
