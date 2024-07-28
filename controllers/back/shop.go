50c1102360e3364ceb306392ff2a86cac39dd648
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-28 06:20:22 +0500

    fix: update CreateShop function

--- controllers/back/shop.go
+++ controllers/back/shop.go
@@ -38,6 +38,13 @@ func CreateShop(c *gin.Context) {
 		return
 	}
 
+	var resizedImage interface{}
+	if shop.Image.String == "" {
+		resizedImage = nil
+	} else {
+		resizedImage = "assets/" + shop.Image.String
+	}
+
 	// eger maglumatlar dogry bolsa onda shops tablisa maglumatlar gosulyar we gosulandan son gosulan maglumatyn id - si return edilyar
 	var shop_id string
 	if err = db.QueryRow(context.Background(),
@@ -45,7 +52,7 @@ func CreateShop(c *gin.Context) {
 		(name_tm,name_ru,address_tm,address_ru,latitude,longitude,image,has_shipping,shop_owner_id,slug_tm,slug_ru,order_number,parent_shop_id,is_shopping_center,resized_image) 
 		VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15) RETURNING id`,
 		shop.NameTM, shop.NameRU, shop.AddressTM, shop.AddressRU, shop.Latitude, shop.Longitude, shop.Image, shop.HasShipping, shop.ShopOwnerID, slug.MakeLang(shop.NameTM, "en"),
-		slug.MakeLang(shop.NameRU, "en"), shop.OrderNumber, shop.ParentShopID, shop.IsShoppingCenter, "assets/"+shop.Image.String).
+		slug.MakeLang(shop.NameRU, "en"), shop.OrderNumber, shop.ParentShopID, shop.IsShoppingCenter, resizedImage).
 		Scan(&shop_id); err != nil {
 		helpers.HandleError(c, 400, err.Error())
 		return
