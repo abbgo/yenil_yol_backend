7aec98bb1b2b960966797c5ed7524280865ef208
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-03 10:40:08 +0500

    fix: update CreateComplaint function

--- controllers/back/complaint.go
+++ controllers/back/complaint.go
@@ -1,7 +1,40 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github/abbgo/yenil_yol/backend/config"
+	"github/abbgo/yenil_yol/backend/helpers"
+	"github/abbgo/yenil_yol/backend/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func CreateComplaint(c *gin.Context) {
+	// initialize database connection
+	db, err := config.ConnDB()
+	if err != nil {
+		helpers.HandleError(c, 400, err.Error())
+		return
+	}
+	defer db.Close()
 
+	// request body - dan gelen maglumatlar alynyar
+	var complaint models.Complaint
+	if err := c.BindJSON(&complaint); err != nil {
+		helpers.HandleError(c, 400, err.Error())
+		return
+	}
+
+	// eger maglumatlar dogry bolsa onda complaints tablisa maglumatlar gosulyar
+	_, err = db.Exec(context.Background(), "INSERT INTO complaints (text_tm,text_ru) VALUES ($1,$2)", complaint.TextTM, complaint.TextRU)
+	if err != nil {
+		helpers.HandleError(c, 400, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  true,
+		"message": "data successfully added",
+	})
 }
