d0ebca307c0070a4cb8294f2eb18e4e130c5c1a1
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-15 06:51:51 +0500

    feat: add RejectedReason column to GetShops serialization struct

--- serializations/shop.go
+++ serializations/shop.go
@@ -24,14 +24,15 @@ type ShopForMapQuery struct {
 }
 
 type GetShops struct {
-	ID            string      `json:"id,omitempty"`
-	NameTM        string      `json:"name_tm,omitempty"`
-	NameRU        string      `json:"name_ru,omitempty"`
-	Latitude      float64     `json:"latitude,omitempty"`
-	Longitude     float64     `json:"longitude,omitempty"`
-	Image         null.String `json:"image,omitempty"`
-	HasShipping   bool        `json:"has_shipping"`
-	CreatedStatus int         `json:"created_status"`
+	ID             string      `json:"id,omitempty"`
+	NameTM         string      `json:"name_tm,omitempty"`
+	NameRU         string      `json:"name_ru,omitempty"`
+	Latitude       float64     `json:"latitude,omitempty"`
+	Longitude      float64     `json:"longitude,omitempty"`
+	Image          null.String `json:"image,omitempty"`
+	HasShipping    bool        `json:"has_shipping,omitempty"`
+	CreatedStatus  int         `json:"created_status,omitempty"`
+	RejectedReason string      `json:"created_status,omitempty"`
 }
 
 type GetShop struct {
